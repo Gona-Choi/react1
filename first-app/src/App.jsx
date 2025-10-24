8585f64ab7417c0b3947f6dad297cf49cbfa4127
Gona Choi <gorani831@gmail.com>
2025-10-25 00:57:33 +1100

    Initial commit - react folder with first-app and weather-app

---
+++ first-app/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import Box from "./component/Box"
+
+
+export default function App(){
+    return(
+        <div>
+            <Box />
+            <Box />
+            <Box />
+        </div>
+    )
+
+};
---
+++ weather-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+//1. 앱이 실행되자마자 현재위치기반의 날씨
+//2. 날씨 정보에는 도시, 섭씨, 화씨 날씨상태
+//3. 5개의 버튼,(1개는 현재, 4개는 다른 도시)
+//4. 도시버튼을 클릭할 때마다 도시별 날씨가 나온다.
+//5. 현재위치 버튼을 누르면 다시 현재위치 기반의 날씨가 나온다.
+//6. 데이터를 들고오는 동안 로딩 스피너가 돈다.
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>hiii</div>
+  )
+}
+
+export default App
